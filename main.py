cd967a7372d0c4ba47e97120598497094ae39795
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-08-20 08:50:26 +0300

    task_12

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-n = int(input('Введите колличество монеток: '))
+# n = int(input('Введите колличество монеток: '))
+# a = 0
+# b = 0
+# for i in range(n):
+#     x = int(input('Орел(1) или решка(0)? '))
+#     if x == 1:
+#         a += 1
+#     else:
+#         b += 1
+# print()
+# if a > b:
+#     print(f'Нужно перевернуть {b} монеток')
+# elif a == b:
+#     print(f'Колличество монет одинаково, по {b} штук')
+# else:
+#     print(f'Нужно перевернуть {a} монеток')
+
+
+s = int(input('Введите сумму чисел: '))
+p = int(input('Введите произведение: '))
 a = 0
-b = 0
-for i in range(n):
-    x = int(input('Орел(1) или решка(0)? '))
-    if x == 1:
-        a += 1
-    else:
-        b += 1
-print()
-if a > b:
-    print(f'Нужно перевернуть {b} монеток')
-elif a == b:
-    print(f'Колличество монет одинаково, по {b} штук')
-else:
-    print(f'Нужно перевернуть {a} монеток')
+for x in range(s):
+    for y in range(s):
+        if x + y == s and x * y == p:
+            a += 1
+            print(x, y)
